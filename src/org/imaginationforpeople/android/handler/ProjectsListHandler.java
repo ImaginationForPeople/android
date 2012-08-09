31ff21096f5424775f2c99763252d43e79bd4096
Cyril Glapa <cyril.glapa@keuse.fr>
2012-08-09 18:02:21 +0200

    Using resources for phrases instead of hard-coding

--- src/org/imaginationforpeople/android/handler/ProjectsListHandler.java
+++ src/org/imaginationforpeople/android/handler/ProjectsListHandler.java
@@ -2,6 +2,7 @@ package org.imaginationforpeople.android.handler;
 
 import java.util.List;
 
+import org.imaginationforpeople.android.R;
 import org.imaginationforpeople.android.model.I4pProjectTranslation;
 
 import android.app.Activity;
@@ -26,9 +27,9 @@ public class ProjectsListHandler extends BaseHandler implements OnClickListener,
 	@Override
 	protected void onStart(int arg, Object obj) {
 		progress = new ProgressDialog(activity);
-		progress.setMessage("Loading projects list");
+		progress.setMessage(activity.getResources().getText(R.string.projectslist_loading));
 		progress.setOnCancelListener(this);
-		progress.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", this);
+		progress.setButton(DialogInterface.BUTTON_NEGATIVE, activity.getResources().getText(R.string.cancel), this);
 		progress.show();
 	}
 	
@@ -50,23 +51,23 @@ public class ProjectsListHandler extends BaseHandler implements OnClickListener,
 		AlertDialog alert = new AlertDialog.Builder(activity).create();
 		switch(arg) {
 		case ERROR_TIMEOUT:
-			alert.setTitle("Problem with the server");
-			alert.setMessage("The server doesn't respond in time.");
+			alert.setTitle(R.string.error_server);
+			alert.setMessage(activity.getResources().getText(R.string.error_server_timeout));
 			break;
 		case ERROR_HTTP:
-			alert.setTitle("Problem with the server");
-			alert.setMessage("We cannot understand server's answer.");
+			alert.setTitle(R.string.error_server);
+			alert.setMessage(activity.getResources().getText(R.string.error_server_badanswer));
 			break;
 		case ERROR_JSON:
-			alert.setTitle("JSON error");
-			alert.setMessage("It seems the server's answer is malformed because we cannot understand the JSON.");
+			alert.setTitle(R.string.error_json);
+			alert.setMessage(activity.getResources().getText(R.string.error_json_badanswer));
 			break;
 		case ERROR_UNKNOWN:
-			alert.setTitle("Unknown error");
-			alert.setMessage("An unhandled error has occured. Please make a bug report :)");
+			alert.setTitle(R.string.error_unknown);
+			alert.setMessage(activity.getResources().getText(R.string.error_unknown_message));
 			break;
 		}
-		alert.setButton(DialogInterface.BUTTON_NEUTRAL, "Close", this);
+		alert.setButton(DialogInterface.BUTTON_NEUTRAL, activity.getResources().getText(R.string.close), this);
 		alert.show();
 	}
 
