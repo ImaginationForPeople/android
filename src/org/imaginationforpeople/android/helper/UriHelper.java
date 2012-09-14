9b30f093ad140e2d34fe745458c51834568d6959
Cyril Glapa <cyril.glapa@keuse.fr>
2012-09-14 18:00:40 +0200

    Rebasing base API URL to a single var, adding a method to get project URL from language code and slug

--- src/org/imaginationforpeople/android/helper/UriHelper.java
+++ src/org/imaginationforpeople/android/helper/UriHelper.java
@@ -1,23 +1,28 @@
 package org.imaginationforpeople.android.helper;
 
 public class UriHelper extends BaseHelper {
+	public final static String API_BASE_URL = "http://10.0.0.5:8000/api/v1";
+	
 	// Public class with private constructor prevent instantiations
 	private UriHelper() {}
 	
 	public static String getBestProjectsListUri() {
-		//TODO: API base URL must be a shared parameter
-		return "http://10.0.0.9:8000/api/v1/project/bestof/?format=json&lang=" + LanguageHelper.getPreferredLanguageCode();
+		return API_BASE_URL + "/project/bestof/?format=json&lang=" + LanguageHelper.getPreferredLanguageCode();
 	}
 	
 	public static String getLatestProjectsListUri() {
-		return "http://10.0.0.9:8000/api/v1/project/latest/?format=json&lang=" + LanguageHelper.getPreferredLanguageCode();
+		return API_BASE_URL + "/project/latest/?format=json&lang=" + LanguageHelper.getPreferredLanguageCode();
 	}
 	
-	public static String getProjectViewUri(int projectId) {
-		return "http://10.0.0.9:8000/api/v1/project/" + String.valueOf(projectId);
+	public static String getProjectViewUriById(int projectId) {
+		return API_BASE_URL + "/project/" + String.valueOf(projectId);
+	}
+	
+	public static String getProjectViewUriBySlug(String language_code, String slug) {
+		return API_BASE_URL + "/project/" + language_code + "/" + slug;
 	}
 	
 	public static String getRandomProjectViewUri() {
-		return "http://10.0.0.9:8000/api/v1/project/random/?format=json&lang=" + LanguageHelper.getPreferredLanguageCode();
+		return API_BASE_URL + "/project/random/?format=json&lang=" + LanguageHelper.getPreferredLanguageCode();
 	}
 }
