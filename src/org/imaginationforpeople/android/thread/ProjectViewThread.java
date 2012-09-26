b3ad868f473db27be62fbd909fb2d052d7ea96c8
Cyril Glapa <cyril.glapa@keuse.fr>
2012-09-26 17:59:58 +0200

    Now parsing images (both thumbnail and big images) when loading a full project

--- src/org/imaginationforpeople/android/thread/ProjectViewThread.java
+++ src/org/imaginationforpeople/android/thread/ProjectViewThread.java
@@ -1,12 +1,17 @@
 package org.imaginationforpeople.android.thread;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 import org.imaginationforpeople.android.handler.ProjectViewHandler;
 import org.imaginationforpeople.android.helper.DataHelper;
 import org.imaginationforpeople.android.helper.UriHelper;
 import org.imaginationforpeople.android.model.I4pProjectTranslation;
+import org.imaginationforpeople.android.model.Picture;
 import org.json.JSONException;
+
+import android.graphics.BitmapFactory;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParseException;
@@ -37,6 +42,15 @@ public class ProjectViewThread extends BaseGetJson {
 		
 		JsonParser parser = factory.createJsonParser(json);
 		I4pProjectTranslation project = mapper.readValue(parser, I4pProjectTranslation.class);
+		
+		if(project.getProject().getPictures().size() > 0) {
+			for(Picture picture : project.getProject().getPictures()) {
+				InputStream URLcontent = (InputStream) new URL(picture.getThumbUrl()).getContent();
+				picture.setThumbBitmap(BitmapFactory.decodeStream(URLcontent));
+				URLcontent = (InputStream) new URL(picture.getImageUrl()).getContent();
+				picture.setImageBitmap(BitmapFactory.decodeStream(URLcontent));
+			}
+		}
 		
 		return project;
 	}
