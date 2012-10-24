b94bf9e5f798732bab5f5b24bda1e636d63d3e24
Cyril Glapa <cyril.glapa@keuse.fr>
2012-10-24 13:04:15 +0200

    Now using the production API

--- src/org/imaginationforpeople/android/helper/UriHelper.java
+++ src/org/imaginationforpeople/android/helper/UriHelper.java
@@ -3,7 +3,7 @@ package org.imaginationforpeople.android.helper;
 import org.imaginationforpeople.android.model.I4pProjectTranslation;
 
 public class UriHelper extends BaseHelper {
-	public final static String BASE_URL = "http://staging.imaginationforpeople.org";
+	public final static String BASE_URL = "http://imaginationforpeople.org";
 	public final static String API_BASE_URL = BASE_URL + "/api/v1";
 	
 	// Public class with private constructor prevent instantiations
