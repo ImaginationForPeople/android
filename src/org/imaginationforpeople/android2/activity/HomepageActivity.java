5020ade0d36bb6a3f760de70ac6555fdd5d631da
Cyril Glapa <cyril.glapa@keuse.fr>
2013-09-13 16:46:46 +0200

    Refactoring of app drawer using Google official drawer (few things missing)

--- src/org/imaginationforpeople/android2/activity/HomepageActivity.java
+++ src/org/imaginationforpeople/android2/activity/HomepageActivity.java
@@ -12,8 +12,33 @@ import org.imaginationforpeople.android2.helper.LanguageHelper;
 import org.imaginationforpeople.android2.model.Group;
 import org.imaginationforpeople.android2.model.I4pProjectTranslation;
 import org.imaginationforpeople.android2.shake.ShakeAnimation;
-import org.imaginationforpeople.android2.shake.ShakeEventListener;
 import org.imaginationforpeople.android2.shake.ShakeAnimation.AnimationListener;
+import org.imaginationforpeople.android2.shake.ShakeEventListener;
+
+import android.app.AlertDialog;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ListView;
+import android.widget.SpinnerAdapter;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.ActionBar.OnNavigationListener;
@@ -23,36 +48,14 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.widget.SearchView;
-import com.darvds.ribbonmenu.OnRibbonChangeListener;
-import com.darvds.ribbonmenu.RibbonMenuView;
-import com.darvds.ribbonmenu.iRibbonMenuCallback;
-
-import android.app.AlertDialog;
-import android.app.SearchManager;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.DialogInterface.OnCancelListener;
-import android.content.DialogInterface.OnClickListener;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.SpinnerAdapter;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class HomepageActivity extends SherlockFragmentActivity implements OnClickListener,
-		OnCancelListener, ShakeEventListener.ShakeListener, AnimationListener,
-		OnCheckedChangeListener, LoadingFragment.OnContentLoadedListener,
-		iRibbonMenuCallback, OnRibbonChangeListener, OnNavigationListener {
+OnCancelListener, ShakeEventListener.ShakeListener, AnimationListener,
+OnCheckedChangeListener, LoadingFragment.OnContentLoadedListener,
+OnNavigationListener, OnItemClickListener {
 	public final static String STATE_KEY = "selected";
-	
-	private ArrayList<ArrayList<I4pProjectTranslation>> projects = new ArrayList<ArrayList<I4pProjectTranslation>>(DataHelper.CONTENT_NUMBER);
+
+	private final ArrayList<ArrayList<I4pProjectTranslation>> projects = new ArrayList<ArrayList<I4pProjectTranslation>>(DataHelper.CONTENT_NUMBER);
 	private ArrayList<Group> groups = new ArrayList<Group>();
 	private AlertDialog languagesDialog;
 	private SharedPreferences preferences;
@@ -60,32 +63,37 @@ public class HomepageActivity extends SherlockFragmentActivity implements OnClic
 	private ShakeEventListener shaker;
 	private ShakeAnimation animation;
 	private SearchView searchView;
-	private RibbonMenuView rbm;
-	private int activeRibonItem = R.id.ribbon_menu_projects;
+	private DrawerLayout drawer;
+	private ListView drawerContent;
+	private ActionBarDrawerToggle drawerToggle;
+	private int activeDrawerItem = 0;
 	private boolean isStopping = false;
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getSupportMenuInflater();
-		switch(activeRibonItem) {
-		case R.id.ribbon_menu_projects:
+		switch(activeDrawerItem) {
+		case 0:
 			inflater.inflate(R.menu.projectslist, menu);
-			
+
 			SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
 			searchView = (SearchView) menu.findItem(R.id.homepage_search).getActionView();
 			searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
 			searchView.setIconifiedByDefault(true);
 			break;
 		}
-		
+
 		return super.onCreateOptionsMenu(menu);
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch(item.getItemId()) {
 		case android.R.id.home:
-			rbm.toggleMenu();
+			if(drawer.isDrawerOpen(drawerContent))
+				drawer.closeDrawer(drawerContent);
+			else
+				drawer.openDrawer(drawerContent);
 			break;
 		case R.id.homepage_content:
 			AlertDialog.Builder contentBuilder = new AlertDialog.Builder(this);
@@ -103,45 +111,7 @@ public class HomepageActivity extends SherlockFragmentActivity implements OnClic
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	@Override
-	public void RibbonMenuItemClick(int itemId) {
-		activeRibonItem = itemId;
-		SherlockFragment fragment;
-		FragmentManager fm = getSupportFragmentManager();
-		switch(itemId) {
-		case R.id.ribbon_menu_projects:
-			initActionBarSpinner();
-			onNavigationItemSelected(0, getSupportActionBar().getSelectedNavigationIndex());
-			
-			supportInvalidateOptionsMenu();
-			break;
-		case R.id.ribbon_menu_workgroups:
-			setTitle(R.string.app_groups);
-			getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-			supportInvalidateOptionsMenu();
-			Bundle data = new Bundle();
-			if(groups.size() > 0) {
-				data.putParcelableArrayList(GroupListFragment.GROUPS_KEY, groups);
-				fragment = new GroupListFragment();
-				fragment.setArguments(data);
-				fm.beginTransaction().replace(R.id.homepage_content, fragment).commit();
-			} else {
-				data.putInt(LoadingFragment.CONTENT_TO_LOAD, LoadingFragment.LOAD_GROUPS);
-				data.putInt(LoadingFragment.TEXT_RESID, R.string.loading_groups);
-				fragment = new LoadingFragment();
-				fragment.setArguments(data);
-				fm.beginTransaction().replace(R.id.homepage_content, fragment).commit();
-			}
-			break;
-		case R.id.ribbon_menu_favorites:
-			getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-			fragment = new FavoritesFragment();
-			fm.beginTransaction().replace(R.id.homepage_content, fragment).commit();
-			break;
-		}
-	}
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -149,46 +119,53 @@ public class HomepageActivity extends SherlockFragmentActivity implements OnClic
 		// -- Initializing application
 		preferences = getPreferences(Context.MODE_PRIVATE);
 		LanguageHelper.setSharedPreferences(preferences);
-		
+
 		// -- Initializing ribbon menu
-		rbm = (RibbonMenuView) findViewById(R.id.homepage_ribbon);
-		rbm.setListener(this);
-		rbm.setMenuClickCallback(this);
-		rbm.setMenuItems(R.menu.homepage_ribbon);
-		rbm.setActiveItem(R.id.ribbon_menu_projects);
-		
+		drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		drawerContent = (ListView) findViewById(R.id.left_drawer);
+
+		String[] drawerItems = getResources().getStringArray(R.array.drawer_items);
+		drawerContent.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, drawerItems));
+		drawerContent.setOnItemClickListener(this);
+
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		
+		getSupportActionBar().setHomeButtonEnabled(true);
+
+		drawerToggle = new ActionBarDrawerToggle(this, drawer, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close);
+		drawer.setDrawerListener(drawerToggle);
+
+		drawerContent.setItemChecked(0, true);
+
 		// -- Initializing projects list
 		if(savedInstanceState != null) {
 			for(int i = 0; i < DataHelper.CONTENT_NUMBER; i++) {
-				ArrayList<I4pProjectTranslation> project = savedInstanceState.getParcelableArrayList("projects_" + String.valueOf(i)); 
-				projects.add(i, project); 
+				ArrayList<I4pProjectTranslation> project = savedInstanceState.getParcelableArrayList("projects_" + String.valueOf(i));
+				projects.add(i, project);
 			}
 			ArrayList<Group> groups = savedInstanceState.getParcelableArrayList("groups");
 			this.groups = groups;
-		} else
+			selectDrawerItem(savedInstanceState.getInt("drawer_item"));
+		} else {
 			for(int i = 0; i < DataHelper.CONTENT_NUMBER; i++) {
 				projects.add(i, new ArrayList<I4pProjectTranslation>());
 			}
-		
-		if(savedInstanceState != null)
-			RibbonMenuItemClick(savedInstanceState.getInt("ribbon_item"));
-		
-		if(activeRibonItem == R.id.ribbon_menu_projects) {
+			selectDrawerItem(0);
+		}
+
+		if(drawerContent.getSelectedItemPosition() == 0) {
 			initActionBarSpinner();
 			if(savedInstanceState != null && savedInstanceState.containsKey(STATE_KEY))
 				getSupportActionBar().setSelectedNavigationItem(savedInstanceState.getInt(STATE_KEY));
 		}
-		
+
 		// -- Initializing language chooser UI
 		int selectedLanguage = LanguageHelper.getPreferredLanguageInt();
-		
+
 		AlertDialog.Builder languagesBuilder = new AlertDialog.Builder(this);
 		languagesBuilder.setTitle(R.string.homepage_spinner_language_prompt);
 		languagesBuilder.setSingleChoiceItems(R.array.homepage_spinner_languages, selectedLanguage, this);
 		languagesDialog = languagesBuilder.create();
-		
+
 		// -- Initializing shaker
 		shaker = new ShakeEventListener(this);
 		if(Build.VERSION.SDK_INT >= 12)
@@ -196,7 +173,7 @@ public class HomepageActivity extends SherlockFragmentActivity implements OnClic
 		else
 			animation = new ShakeAnimation(this, findViewById(R.id.homepage_shake), this);
 	}
-	
+
 	@Override
 	protected void onRestart() {
 		super.onRestart();
@@ -206,7 +183,7 @@ public class HomepageActivity extends SherlockFragmentActivity implements OnClic
 			searchView.setIconified(true);
 		}
 	}
-	
+
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		isStopping = true;
@@ -214,7 +191,7 @@ public class HomepageActivity extends SherlockFragmentActivity implements OnClic
 			outState.putParcelableArrayList("projects_" + String.valueOf(i), projects.get(i));
 		outState.putParcelableArrayList("groups", groups);
 		outState.putInt(STATE_KEY, getSupportActionBar().getSelectedNavigationIndex());
-		outState.putInt("ribbon_item", activeRibonItem);
+		outState.putInt("drawer_item", activeDrawerItem);
 		super.onSaveInstanceState(outState);
 	}
 
@@ -236,15 +213,20 @@ public class HomepageActivity extends SherlockFragmentActivity implements OnClic
 		if(languagesDialog.isShowing())
 			languagesDialog.cancel();
 	}
-	
+
 	@Override
-	public void onBackPressed() {
-		if(rbm.isMenuVisible())
-			rbm.hideMenu();
-		else
-			super.onBackPressed();
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		drawerToggle.syncState();
 	}
 
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		drawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
 	public void onClick(DialogInterface dialog, int which) {
 		if(which == DialogInterface.BUTTON_NEGATIVE) {
 			finish();
@@ -256,35 +238,39 @@ public class HomepageActivity extends SherlockFragmentActivity implements OnClic
 		}
 		dialog.dismiss();
 	}
-	
+
+	@Override
 	public void onCancel(DialogInterface arg0) {
 		finish();
 	}
-	
+
 	public void resetProjects() {
 		for(ArrayList<I4pProjectTranslation> array : projects)
 			array.clear();
 		onNavigationItemSelected(0, getSupportActionBar().getSelectedNavigationIndex());
 	}
 
+	@Override
 	public void onShake() {
 		Intent intent = new Intent(this, ProjectViewActivity.class);
 		intent.putExtra("project_id", DataHelper.PROJECT_RANDOM);
 		startActivity(intent);
 	}
-	
+
+	@Override
 	public void onLittleShake() {
 		if(preferences.getBoolean("shake_hint", true))
 			animation.showAnimation();
 	}
-	
+
+	@Override
 	public void onClick() {
 		animation.hideAnimation();
-		
+
 		View shakeLayout = getLayoutInflater().inflate(R.layout.shake_dialog, null);
 		CheckBox shakeCheckbox = (CheckBox) shakeLayout.findViewById(R.id.shake_checkbox);
 		shakeCheckbox.setOnCheckedChangeListener(this);
-		
+
 		AlertDialog.Builder shakeAlert = new AlertDialog.Builder(this);
 		shakeAlert.setIcon(android.R.drawable.ic_dialog_info);
 		shakeAlert.setTitle(R.string.shake_dialog_title);
@@ -292,7 +278,8 @@ public class HomepageActivity extends SherlockFragmentActivity implements OnClic
 		shakeAlert.setPositiveButton(android.R.string.ok, null);
 		shakeAlert.create().show();
 	}
-	
+
+	@Override
 	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 		Editor editor = preferences.edit();
 		editor.putBoolean("shake_hint", !isChecked);
@@ -323,22 +310,12 @@ public class HomepageActivity extends SherlockFragmentActivity implements OnClic
 			return;
 		fm.beginTransaction().replace(R.id.homepage_content, fragment).commit();
 	}
-	
-	@Override
-	public void onRibbonOpen() {
-		getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-	}
-	
-	@Override
-	public void onRibbonClose() {
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-	}
 
 	@Override
 	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
 		int id = (int) itemId;
 		setTitle("");
-		
+
 		Bundle data = new Bundle();
 		data.putParcelableArrayList(ProjectListFragment.PROJECTS_KEY, projects.get(id));
 		SherlockFragment fragment;
@@ -362,11 +339,55 @@ public class HomepageActivity extends SherlockFragmentActivity implements OnClic
 		fm.beginTransaction().replace(R.id.homepage_content, fragment).commit();
 		return true;
 	}
-	
+
 	private void initActionBarSpinner() {
 		SpinnerAdapter spinner = ArrayAdapter.createFromResource(this, R.array.homepage_spinner_dropdown, com.actionbarsherlock.R.layout.sherlock_spinner_dropdown_item);
-		
+
 		getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
 		getSupportActionBar().setListNavigationCallbacks(spinner, this);
+	}
+
+	private void selectDrawerItem(int position) {
+		activeDrawerItem = position;
+		SherlockFragment fragment;
+		FragmentManager fm = getSupportFragmentManager();
+		switch(position) {
+		case 0:
+			initActionBarSpinner();
+			onNavigationItemSelected(0, getSupportActionBar().getSelectedNavigationIndex());
+
+			supportInvalidateOptionsMenu();
+			break;
+		case 1:
+			setTitle(R.string.app_groups);
+			getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+			supportInvalidateOptionsMenu();
+			Bundle data = new Bundle();
+			if(groups.size() > 0) {
+				data.putParcelableArrayList(GroupListFragment.GROUPS_KEY, groups);
+				fragment = new GroupListFragment();
+				fragment.setArguments(data);
+				fm.beginTransaction().replace(R.id.homepage_content, fragment).commit();
+			} else {
+				data.putInt(LoadingFragment.CONTENT_TO_LOAD, LoadingFragment.LOAD_GROUPS);
+				data.putInt(LoadingFragment.TEXT_RESID, R.string.loading_groups);
+				fragment = new LoadingFragment();
+				fragment.setArguments(data);
+				fm.beginTransaction().replace(R.id.homepage_content, fragment).commit();
+			}
+			break;
+		case 2:
+			getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+			fragment = new FavoritesFragment();
+			fm.beginTransaction().replace(R.id.homepage_content, fragment).commit();
+			break;
+		}
+		drawerContent.setItemChecked(position, true);
+		drawer.closeDrawer(drawerContent);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		selectDrawerItem(position);
 	}
 }
