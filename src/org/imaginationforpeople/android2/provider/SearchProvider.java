30d7691cc3939abb0adcb87847edde37ab7355b1
Cyril Glapa <cyril.glapa@keuse.fr>
2013-09-25 15:26:40 +0200

    Correcting encoding for network data

--- src/org/imaginationforpeople/android2/provider/SearchProvider.java
+++ src/org/imaginationforpeople/android2/provider/SearchProvider.java
@@ -8,14 +8,11 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 import org.imaginationforpeople.android2.helper.UriHelper;
 import org.imaginationforpeople.android2.model.I4pProjectTranslation;
 import org.json.JSONArray;
-
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import android.app.SearchManager;
 import android.content.ContentProvider;
@@ -25,6 +22,10 @@ import android.database.MatrixCursor;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class SearchProvider extends ContentProvider {
 	public static final String[] columns = {
 		BaseColumns._ID,
@@ -32,7 +33,7 @@ public class SearchProvider extends ContentProvider {
 		SearchManager.SUGGEST_COLUMN_INTENT_DATA,
 		SearchManager.SUGGEST_COLUMN_INTENT_EXTRA_DATA
 	};
-	
+
 	@Override
 	public int delete(Uri uri, String selection, String[] selectionArgs) {
 		return 0;
@@ -57,35 +58,35 @@ public class SearchProvider extends ContentProvider {
 	public Cursor query(Uri contentUri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 		String query = contentUri.getLastPathSegment().toLowerCase();
 		MatrixCursor projects = new MatrixCursor(columns);
-		
+
 		// If the text field is empty, do not try to search projects
 		if(SearchManager.SUGGEST_URI_PATH_QUERY.equals(query))
 			return projects;
-		
+
 		BasicHttpParams basicHttpParams = new BasicHttpParams();
 		// Connection must time out after 10 second to prevent infinite loop
 		HttpConnectionParams.setConnectionTimeout(basicHttpParams, 10000);
 		HttpClient httpClient = new DefaultHttpClient(basicHttpParams);
-		
+
 		HttpGet httpGet = new HttpGet();
 		httpGet.setHeader("Accept", "application/json");
 		try {
 			URI uri = new URI(UriHelper.getQuickSearchUrl(query));
 			httpGet.setURI(uri);
 			HttpResponse response = httpClient.execute(httpGet);
-			
+
 			JsonFactory factory = new JsonFactory();
 			ObjectMapper mapper = new ObjectMapper();
-			
-			JSONArray jsonProjects = new JSONArray(EntityUtils.toString(response.getEntity()));
-			
+
+			JSONArray jsonProjects = new JSONArray(EntityUtils.toString(response.getEntity(), HTTP.UTF_8));
+
 			int jsonLength = jsonProjects.length();
 			for(int i = 0; i < jsonLength; i++) {
 				JsonParser parser = factory.createJsonParser(jsonProjects.getString(i));
 				I4pProjectTranslation project = mapper.readValue(parser, I4pProjectTranslation.class);
 				projects.addRow(new String[]{String.valueOf(i), project.getTitle(), project.getLanguageCode() + "/" + project.getSlug(), project.getTitle()});
 			}
-			
+
 			return projects;
 		} catch (Exception e) {
 			e.printStackTrace();
--- src/org/imaginationforpeople/android2/thread/BaseGetJson.java
+++ src/org/imaginationforpeople/android2/thread/BaseGetJson.java
@@ -12,46 +12,47 @@ import org.apache.http.conn.HttpHostConnectException;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 import org.imaginationforpeople.android2.handler.BaseHandler;
 import org.imaginationforpeople.android2.helper.ErrorHelper;
 import org.json.JSONException;
 
-import com.fasterxml.jackson.core.JsonParseException;
-
 import android.os.Message;
 import android.util.Log;
 
+import com.fasterxml.jackson.core.JsonParseException;
+
 public abstract class BaseGetJson extends Thread {
 	private boolean stop = false;
-	
+
 	protected BaseHandler handler;
 	protected String requestUri;
 	protected int arg;
-	
+
 	public void requestStop() {
 		stop = true;
 	}
-	
+
 	protected boolean isStopped() {
 		return stop;
 	}
-	
+
 	@Override
 	public void run() {
 		if(stop)
 			return;
-		
+
 		Message msg;
 		msg = handler.obtainMessage();
 		msg.arg1 = BaseHandler.STATUS_START;
 		msg.arg2 = arg;
 		handler.sendMessage(msg);
-		
+
 		String json;
 		try {
 			onStart();
-			
+
 			json = sendRequest();
 			if(stop)
 				return;
@@ -85,28 +86,28 @@ public abstract class BaseGetJson extends Thread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	protected abstract void onStart()
 			throws IOException;
-	
+
 	private String sendRequest() throws ConnectTimeoutException, HttpHostConnectException, Exception {
 		BasicHttpParams basicHttpParams = new BasicHttpParams();
 		// Connection must time out after 10 second to prevent infinite loop
 		HttpConnectionParams.setConnectionTimeout(basicHttpParams, 10000);
 		HttpClient httpClient = new DefaultHttpClient(basicHttpParams);
-		
+
 		HttpGet httpGet = new HttpGet();
 		httpGet.setHeader("Accept", "application/json");
 		URI uri = new URI(requestUri);
 		httpGet.setURI(uri);
-		
+
 		HttpResponse response = httpClient.execute(httpGet);
-		return EntityUtils.toString(response.getEntity()); 
+		return EntityUtils.toString(response.getEntity(), HTTP.UTF_8);
 	}
-	
+
 	protected abstract Object parseJson(String json)
 			throws JSONException, JsonParseException, IOException;
-	
+
 	protected void sendError(int errorCode) {
 		if(stop)
 			return;
@@ -115,7 +116,7 @@ public abstract class BaseGetJson extends Thread {
 		msg.arg2 = errorCode;
 		handler.sendMessage(msg);
 	}
-	
+
 	protected InputStream download(URI uri) throws IOException {
 		HttpClient httpClient = new DefaultHttpClient();
 		HttpGet httpGet = new HttpGet();
